0674054304edc8175b42179522b77b2b25826094
toancse <minhtoanaglx@gmail.com>
2024-04-26 23:05:20 +0700

    update unitest

--- main.py
+++ main.py
@@ -1,17 +1,13 @@
-from pake import *
-from func1 import *
-from func2 import *
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import Select
-import time
+import unittest
+from test_search import PythonOrgSearch
+from test_abc import PythonOrgSearchABC
 
+if __name__ == "__main__":
+    # Create test suite
+    suite = unittest.TestSuite()
 
-# Gọi các hàm test từ func1.py
-test_CP002008()
-
-
-# Quit the WebDriver
-driver.quit()
+    # Add test cases from different files to the suite
+    suite.addTest(unittest.makeSuite(PythonOrgSearch))
+    suite.addTest(unittest.makeSuite(PythonOrgSearchABC))
+    # Run the test suite
+    unittest.TextTestRunner(verbosity=2).run(suite)
